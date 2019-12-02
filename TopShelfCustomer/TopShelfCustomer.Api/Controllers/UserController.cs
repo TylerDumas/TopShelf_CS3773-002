1fdc11e179c472e56eea0b45e0c853da03c984cd
Jackson Dumas <tylerdumas3@hotmail.com>
2019-12-02 11:34:18 -0600

    Updated NuGet packages for web api and added User model and controller

--- TopShelfCustomer/TopShelfCustomer.Api/Controllers/UserController.cs
+++ TopShelfCustomer/TopShelfCustomer.Api/Controllers/UserController.cs
@@ -4,13 +4,38 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using Newtonsoft.Json;
+using TopShelfCustomer.Api.Models;
 
 namespace TopShelfCustomer.Api.Controllers {
+
+    /// <summary>
+    /// UserController:
+    /// 
+    /// 
+    /// </summary>
     public class UserController : ApiController {
+
+        public List<User> users = new List<User>();
+
+        public UserController () {
+            users.Add( new Models.User {
+                Id = 1,
+                FullName = "Jackson Dumas",
+                EmailAddress = "tylerdumas3@hotmail.com", 
+                PhoneNumber = "210-464-3176", 
+                StreetAddress = "6676 UTSA Boulevard", 
+                StoreName = "HEB DeZavala"
+            } );
+        }
 
         // GET: api/User
         public IEnumerable<string> Get() {
-            return new string[] { "value1", "value2" };
+            List<string> ret = new List<string>();
+            foreach( User user in users ) {
+                ret.Add( JsonConvert.SerializeObject( user ) );
+            }
+            return ret;
         }
 
         // GET: api/User/5
---
+++ TopShelfCustomer/TopShelfCustomer.Api/Models/User.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TopShelfCustomer.Api.Models {
+
+    /// <summary>
+    /// User:
+    /// 
+    /// API Model for a user entry in the database.
+    /// All API requests to the /api/User endpoint 
+    /// will return this as a JSON string object.
+    /// </summary>
+    public class User {
+
+        public int Id { get; set; } = 0;
+
+        public string FullName { get; set; } = "";
+
+        public string EmailAddress { get; set; } = "";
+
+        public string PhoneNumber { get; set; } = "";
+
+        public string StreetAddress { get; set; } = "";
+
+        public string StoreName { get; set; } = "";
+
+    }
+}
