a2493bde6cb49a5aa7e5bb0250be8a5c821a4189
Jackson Dumas <tylerdumas3@hotmail.com>
2019-12-02 13:20:06 -0600

    Added SqlDataAccess class and xml docs for api

--- TopShelfCustomer/TopShelfCustomer.Api/Controllers/UserController.cs
+++ TopShelfCustomer/TopShelfCustomer.Api/Controllers/UserController.cs
@@ -12,50 +12,118 @@ namespace TopShelfCustomer.Api.Controllers {
     /// <summary>
     /// UserController:
     /// 
-    /// 
+    /// Controller for the User collection.
+    /// Contains functionality for all RESTful API calls
+    /// such as: Get, Put, Post, and Delete.
     /// </summary>
     public class UserController : ApiController {
-
+        
+        /// <summary>
+        /// users: Re-usable list of Users to allow fetching from database.
+        /// </summary>
         public List<User> users = new List<User>();
+        /// <summary>
+        /// user: Re-usable User to allow fetching from database.
+        /// </summary>
+        public User user = new User();
 
+        /// <summary>
+        /// Constructor
+        /// </summary>
         public UserController () {
-            users.Add( new Models.User {
+            users.Add( new User {
                 Id = 1,
                 FullName = "Jackson Dumas",
                 EmailAddress = "tylerdumas3@hotmail.com", 
                 PhoneNumber = "210-464-3176", 
                 StreetAddress = "6676 UTSA Boulevard", 
                 StoreName = "HEB DeZavala"
+            });
+            users.Add( new User {
+                Id = 2,
+                FullName = "Dylan Dumas",
+                EmailAddress = "dylandumas@gmail.com",
+                PhoneNumber = "210-289-7527",
+                StreetAddress = "6699 YoMomma Avenue",
+                StoreName = "HEB Bulverde and 1604"
             } );
         }
 
-        // GET: api/User
-        public IEnumerable<string> Get() {
-            List<string> ret = new List<string>();
-            foreach( User user in users ) {
-                ret.Add( JsonConvert.SerializeObject( user ) );
+        /// <summary>
+        /// GetAllUsers:
+        /// 
+        /// Gets the entire list of Users.
+        /// FIXME: this will be removed in the future.
+        /// </summary>
+        /// <returns> The entire User list </returns>
+        [Route( "api/User/GetAllUsers" )]
+        [HttpGet]
+        public IEnumerable<User> Get() {
+            return users;
+        }
+
+        /// <summary>
+        /// GetUserById:
+        /// 
+        /// Overload of the Get request method.
+        /// Fetches a single user from the API with the
+        /// matching Id property. Matches based off of the passed
+        /// id argument.
+        /// </summary>
+        /// <param name="userId"> The id of the requested User </param>
+        /// <returns> a User with a matching Id </returns>
+        [Route( "api/User/GetUserById/{userId:int}")]
+        [HttpGet]
+        public User Get( int userId ) {
+            return users.Where( x => x.Id == userId ).FirstOrDefault();
+        }
+
+        /// <summary>
+        /// Post:
+        /// 
+        /// Takes a User object as an argument and
+        /// adds it to the List of users. Adds
+        /// append functionality to the API.
+        /// </summary>
+        /// <param name="value"> The User to add </param>
+        [Route( "api/User/PostUser" )]
+        [HttpPost]
+        public void Post( [FromBody]User value ) {
+            users.Add( value );
+        }
+
+        /// <summary>
+        /// Put:
+        /// 
+        /// Takes a User object as an argument and updates an
+        /// entry if it exists. If not, it appends to the list
+        /// of Users.
+        /// </summary>
+        /// <param name="id"> The Id value of the User object </param>
+        /// <param name="value"> The User to add </param>
+        [Route( "api/User/PutUser" )]
+        [HttpPut]
+        public void Put( int id, [FromBody]User value ) {
+            User temp = users.Where( x => x.Id == value.Id ).FirstOrDefault();
+            if ( temp != null ) {
+                users.Remove( temp );
+                users.Add( value );
+            } else {
+                users.Add( value );
             }
-            return ret;
         }
 
-        // GET: api/User/5
-        public string Get( int id ) {
-            return "value";
-        }
-
-        // POST: api/User
-        public void Post( [FromBody]string value ) {
-
-        }
-
-        // PUT: api/User/5
-        public void Put( int id, [FromBody]string value ) {
-
-        }
-
-        // DELETE: api/User/5
-        public void Delete( int id ) {
-
+        /// <summary>
+        /// Delete:
+        /// 
+        /// Deletes the User entry that matches the passed id.
+        /// Matches based off of the Id property on the User.
+        /// </summary>
+        /// <param name="userId"> The Id value to match </param>
+        [Route( "api/User/DeleteUser/{userId:int}" )]
+        [HttpDelete]
+        public void Delete( int userId ) {
+            users.Remove( users.Where( x => x.Id == userId ).FirstOrDefault() );
         }
     }
 }
---
+++ TopShelfCustomer/TopShelfCustomer.Api/Services/SqlDataAccess.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Configuration;
+using Dapper;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace TopShelfCustomer.Api.Services {
+
+    /// <summary>
+    /// SqlDataAccess:
+    /// 
+    /// Helper class to allow for access to the TopShelf
+    /// database. Contains functionality for reading/writing to
+    /// the database.
+    /// </summary>
+    public class SqlDataAccess {
+
+        /// <summary>
+        /// GetConnectionString:
+        /// 
+        /// Helper method to return the value of the connection string,
+        /// within the configuration manager, at index "name".
+        /// </summary>
+        /// <param name="name"> The key for the connection string value </param>
+        /// <returns> the value of the connection string for "name" </returns>
+        public string GetConnectionString( string name ) {
+            return ConfigurationManager.ConnectionStrings[name].ConnectionString;
+        }
+
+        /// <summary>
+        /// LoadData:
+        /// 
+        /// Acts as a read method for the database.
+        /// </summary>
+        /// <typeparam name="T"> The object type to load from the database </typeparam>
+        /// <typeparam name="U"> The type of the parameters object to query with </typeparam>
+        /// <param name="storedProcedure"> The SQL procedure to use on the database </param>
+        /// <param name="parameters"> The parameters for the SQL query </param>
+        /// <param name="connectionStringName"> The shortened name of the SQL connection string </param>
+        /// <returns> A list of Type T objects returned from the database query </returns>
+        public List<T> LoadData<T, U>( string storedProcedure, U parameters, string connectionStringName ) {
+
+            string connectionString = GetConnectionString( connectionStringName );      //The full connection string
+
+            using( IDbConnection connection = new SqlConnection( connectionString ) ) {     //Database connection and Query
+                List<T> rows = connection.Query<T>( storedProcedure, parameters,
+                    commandType: CommandType.StoredProcedure ).ToList();
+                return rows;
+            }
+        }
+
+        /// <summary>
+        /// SaveData:
+        /// 
+        /// Acts as a write method for the database.
+        /// </summary>
+        /// <typeparam name="T"> The type of the parameters passed to the SQL query </typeparam>
+        /// <param name="storedProcedure"> The SQL procedure to use on the database </param>
+        /// <param name="parameters"> The parameters for the SQL query </param>
+        /// <param name="connectionStringName"> The shortened name of the SQL connection string </param>
+        public void SaveData<T>( string storedProcedure, T parameters, string connectionStringName ) {
+
+            string connectionString = GetConnectionString( connectionStringName );      //The full connection string
+
+            using ( IDbConnection connection = new SqlConnection( connectionString ) ) {        //Database connection and Query
+                connection.Execute( storedProcedure,
+                    parameters, commandType: CommandType.StoredProcedure );
+            }
+        }
+    }
+}
